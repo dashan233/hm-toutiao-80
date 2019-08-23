5450cb86788b7e73479793e153cc588f5a08876c
liuhaishan <913138845@qq.com>
2019-08-23 21:19:09 +0800

    完成home基础布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    { path: '/', redirect: '/home' },
+    { path: '/home', name: 'home', component: Home },
+    { path: '/login', name: 'login', component: Login }
   ]
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container class="my-container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.my-container {
+    width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .el-aside{
+    background: #002033;
+  }
+  .el-header{
+    border-bottom: 1px solid #ddd;
+  }
+}
+</style>
