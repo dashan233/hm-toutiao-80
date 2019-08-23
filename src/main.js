a51661bb8123a2629ab58756704d394ecce2974a
liuhaishan <913138845@qq.com>
2019-08-23 20:51:40 +0800

    完成login登录校验

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from '@/router'
+import axios from 'axios'
 
 Vue.use(ElementUi)
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="container">
+  <div class="container" ref="box">
     <el-card class="my-card">
       <img src="../../assets/images/logo_index.png" alt />
-      <el-form :model="loginForm">
-        <el-form-item style="margin-top:20px">
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+        <el-form-item style="margin-top:20px" prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item >
-          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:66%;margin-right:10px"></el-input>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:66%;margin-right:10px"
+          ></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
         <el-form-item>
-          <el-checkbox :value="true" >我已阅读并同意用户协议和隐私条款</el-checkbox>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form>
-            <el-button type="primary" style="width:100%">登 录</el-button>
+          <el-button @click="login()" type="primary" style="width:100%">登 录</el-button>
         </el-form>
       </el-form>
     </el-card>
@@ -24,11 +28,50 @@
 <script>
 export default {
   data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号不合法'))
+      }
+      callback()
+    }
+
+    const checkCode = (rule, value, callback) => {
+      if (!/\d{5}[0-9]$/.test(value)) {
+        return callback(new Error('验证码不合法'))
+      }
+      callback()
+    }
     return {
       loginForm: {
         mobile: '',
         code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码长度6位', trigger: 'blur' },
+          { validator: checkCode, trigger: 'change' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或者验证码有误')
+            })
+        }
+      })
     }
   }
 }
