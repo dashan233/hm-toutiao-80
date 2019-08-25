57e70aa12fdc1a41dc778fde2f4fae5ba2445778
liuhaishan <913138845@qq.com>
2019-08-25 11:42:47 +0800

    完成页面导航模块和头部内容

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
 import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', redirect: '/home' },
-    { path: '/home', name: 'home', component: Home },
+    { path: '/',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    },
     { path: '/login', name: 'login', component: Login }
   ]
 })
