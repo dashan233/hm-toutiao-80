9c0f8cc01120854d570011487c2961d15b8d224e
liuhaishan <913138845@qq.com>
2019-09-02 14:06:31 +0800

    评论管理--基础布局

---
+++ src/views/comment/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <el-card>
+            <div slot="header">
+                <my-bread>评论管理</my-bread>
+            </div>
+            <!-- 表格 -->
+            <el-table>
+                <el-table-column label="标题"></el-table-column>
+                <el-table-column label="总评论数"></el-table-column>
+                <el-table-column label="粉丝评论数"></el-table-column>
+                <el-table-column label="状态"></el-table-column>
+                <el-table-column label="操作"></el-table-column>
+            </el-table>
+            <!-- 分页 -->
+            <el-pagination background layout="prev,pager,next" :total="1000"></el-pagination>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      articles: []
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
